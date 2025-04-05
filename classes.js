0bbaa006834922fe825d0c6d69287993367ae32d
izijerry <izijerry56@gmail.com>
2025-04-05 21:40:02 +0700

    transition back to map
    bug di battle scene karakter pindah posisi saat mati

--- classes.js
+++ classes.js
@@ -9,12 +9,14 @@ class Sprite {
     
     }) {
         this.position = position
-        this.image = image
+        this.image = new Image()
         this.frames = { ...frames, val: 0, elapsed: 0 }
         this.image.onload = () => {
             this.width = this.image.width / this.frames.max
             this.height = this.image.height
         }
+        this.image.src = image.src
+        
         this.animate = animate
         this.sprites = sprites
         this.opacity = 1
@@ -80,6 +82,17 @@ class Monster extends Sprite {
         this.name = name
         this.attacks = attacks       
     }
+    
+    faint(){
+        document.querySelector('#dialogueBox').innerHTML = this.name + ' fainted! '
+        gsap.to(this.position,{
+            y: this.position.y + 20
+        }) 
+        gsap.to(this, {
+            opacity: 0
+        })
+    }
+
     attack({ attack, recipient, renderedSprites }) {
         document.querySelector('#dialogueBox').style.display = 'block'
         document.querySelector('#dialogueBox').innerHTML = this.name + ' used ' + attack.name
@@ -90,7 +103,7 @@ class Monster extends Sprite {
         let rotation = 1
         if (this.isEnemy) rotation = -2.2
 
-        this.health -= attack.damage
+        recipient.health -= attack.damage
 
 
         switch (attack.name) {
@@ -119,7 +132,7 @@ class Monster extends Sprite {
                     onComplete: () => {
                         // Enemy actually gets hit
                         gsap.to(healthBar, {
-                            width: this.health + '%'
+                            width: recipient.health + '%'
                         })
                         gsap.to(recipient.position, {
                             x: recipient.position.x + 10,
@@ -156,7 +169,7 @@ class Monster extends Sprite {
                         onComplete: () => {
                             // Enemy actually gets hit
                             gsap.to(healthBar, {
-                                width: this.health + '%'
+                                width: recipient.health + '%'
                             })
                             gsap.to(recipient.position, {
                                 x: recipient.position.x + 10,
